8da9c5b95a42a85ff82f48b8b4032c5bb859fef7
Alex <aasmc@yandex.ru>
2023-03-29 10:42:21 +0300

    feat: implement BookClient onErrorResume, add some tests to the BookClient

--- src/main/java/ru/aasmc/orderservice/book/BookClient.java
+++ src/main/java/ru/aasmc/orderservice/book/BookClient.java
@@ -2,6 +2,7 @@ package ru.aasmc.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -23,6 +24,11 @@ public class BookClient {
                 .retrieve()
                 .bodyToMono(Book.class)
                 .timeout(Duration.ofSeconds(3), Mono.empty())
+                // return Mono.empty() in case of 404 error, and not
+                // proceed with retries
+                .onErrorResume(
+                        WebClientResponseException.NotFound.class,
+                        exception -> Mono.empty())
                 // Placing retryWhen() after timeout(), means that the timeout is applied
                 // to each retry attempt. Placing it after timeout() means the timeout
                 // is applied to the overall operation (i.e. the whole sequence of initial
@@ -34,6 +40,12 @@ public class BookClient {
                         // When you have multiple instances of Order Service running, the jitter
                         // factor ensures that the replicas will not retry requests simultaneously
                         Retry.backoff(3, Duration.ofMillis(100))
+                )
+                // If any error happens after the 3 retry attempts, catch the exception and
+                // return an empty object
+                .onErrorResume(
+                        Exception.class,
+                        e -> Mono.empty()
                 );
     }
 }
